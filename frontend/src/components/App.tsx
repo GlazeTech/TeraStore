8b6faaa87eb89e55ad386860644d5e7e14023ce7
Bjørn Mølvig <bjoernmoelvig@gmail.com>
2023-10-11 15:59:23 +0200

    added formatting

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -3,21 +3,21 @@ import { pingBackend } from "api";
 import "assets/App.css";
 import { useState } from "react";
 function App() {
-  const [backendMsg, setBackendMsg] = useState<string>("");
+	const [backendMsg, setBackendMsg] = useState<string>("");
 
-  const handleButtonClick = () => {
-    pingBackend().then((msg) => setBackendMsg(msg));
-  };
+	const handleButtonClick = () => {
+		pingBackend().then((msg) => setBackendMsg(msg));
+	};
 
-  return (
-    <div>
-      <h1>Hello, world!</h1>
-      <Button onClick={handleButtonClick} color="primary" variant="outlined">
-        Click me
-      </Button>
-      <h2>{backendMsg}</h2>
-    </div>
-  );
+	return (
+		<div>
+			<h1>Hello, world!</h1>
+			<Button onClick={handleButtonClick} color="primary" variant="outlined">
+				Click me
+			</Button>
+			<h2>{backendMsg}</h2>
+		</div>
+	);
 }
 
 export default App;
