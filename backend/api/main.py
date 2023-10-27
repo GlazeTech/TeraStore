6f248ace5c302aad372d0ffeb6edd8e443b6c467
Mads Ehrhorn <ehrhorn@me.com>
2023-10-27 11:08:22 +0200

    Prod does not create tables (#44)

--- backend/api/main.py
+++ backend/api/main.py
@@ -7,19 +7,32 @@ from fastapi.middleware.cors import CORSMiddleware
 from api.database import create_db_and_tables, drop_tables
 from api.public import make_api
 from api.utils.mock_data_generator import create_devices_and_pulses
-from api.utils.types import WithLifespan
+from api.utils.types import Lifespan
 
 
 @asynccontextmanager
-async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
+async def lifespan_dev(app: FastAPI) -> AsyncGenerator[None, None]:
     create_db_and_tables()
     create_devices_and_pulses()
     yield
     drop_tables()
 
 
-def create_app(with_lifespan: WithLifespan) -> FastAPI:
-    app = FastAPI(lifespan=lifespan) if with_lifespan.value else FastAPI()
+@asynccontextmanager
+async def lifespan_prod(app: FastAPI) -> AsyncGenerator[None, None]:
+    create_db_and_tables()
+    yield
+
+
+LIFESPAN_FUNCTIONS = {
+    Lifespan.PROD: lifespan_prod,
+    Lifespan.DEV: lifespan_dev,
+    Lifespan.TEST: lifespan_dev,
+}
+
+
+def create_app(lifespan: Lifespan) -> FastAPI:
+    app = FastAPI(lifespan=LIFESPAN_FUNCTIONS[lifespan])
     app.add_middleware(
         CORSMiddleware,
         allow_origins=["*"],
