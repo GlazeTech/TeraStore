2df721e1d82af31c5a3ec36ae7d83bf64a9ac45a
Bjørn Mølvig <bjoernmoelvig@gmail.com>
2023-10-30 11:35:24 +0100

    Added test to get 90 % coverage; added description of ruff rules; added missing dot in hashfile filename

--- backend/api/utils/mock_data_generator.py
+++ backend/api/utils/mock_data_generator.py
@@ -15,6 +15,36 @@ def create_devices_and_pulses() -> None:
         device_g_2 = create_device(DeviceCreate.create_mock("Glaze II"), sess)
         device_carmen = create_device(DeviceCreate.create_mock("Carmen"), sess)
 
+        pulse_1 = create_pulse(
+            PulseCreate.create_mock(device_id=device_g_1.device_id),
+            sess,
+        )
+        pulse_2 = create_pulse(
+            PulseCreate.create_mock(device_id=device_g_2.device_id),
+            sess,
+        )
+        pulse_3 = create_pulse(
+            PulseCreate.create_mock(device_id=device_carmen.device_id),
+            sess,
+        )
+
+        add_str_attr(pulse_1.pulse_id, "angle", "29", sess)
+        add_str_attr(pulse_1.pulse_id, "substrate", "sand-blasted steel", sess)
+
+        add_str_attr(pulse_2.pulse_id, "angle", "23", sess)
+        add_str_attr(pulse_2.pulse_id, "substrate", "plastic", sess)
+
+        add_str_attr(pulse_3.pulse_id, "angle", "17", sess)
+        add_str_attr(pulse_3.pulse_id, "substrate", "polymer", sess)
+
+
+def create_frontend_dev_data() -> None:
+    """Create devices and pulses for testing purposes."""
+    with Session(app_engine) as sess:
+        device_g_1 = create_device(DeviceCreate.create_mock("Glaze I"), sess)
+        device_g_2 = create_device(DeviceCreate.create_mock("Glaze II"), sess)
+        device_carmen = create_device(DeviceCreate.create_mock("Carmen"), sess)
+
         projects = {
             "CGM": 5,
             "graphene": 8,
